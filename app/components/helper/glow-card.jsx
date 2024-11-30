a4dfcb3d4393fa8e6ebf5b86e27d1bc622a0ca6b
YashSoni321 <yashsonijaipur@gmail.com>
2024-11-30 23:31:05 +0530

    changes in website

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -1,5 +1,7 @@
 "use client"
+import React from 'react';
 import { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 
 const GlowCard = ({ children , identifier}) => {
   useEffect(() => {
