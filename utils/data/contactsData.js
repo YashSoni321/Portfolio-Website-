e27a2483b824ec8a561a1f0a3e9d575b7fcbc713
YashSoni321 <yashsonijaipur@gmail.com>
2025-01-19 22:08:31 +0530

    changes in test

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,8 +1,6 @@
 
 
 export const contactsData = {
-<<<<<<< HEAD
-=======
     name: "Yash Soni",
     profile: "/profile_pic.jpg",
     designation: "Software Engineer",
@@ -11,19 +9,10 @@ export const contactsData = {
   stack (MongoDB, Express.js, React.js, Angular, Node.js). Proficient in
   mobile development (iOS, Android) and DevOps, delivering robust, scalable, user-centric solutions. Skilled in seamless frontend- backend integration, optimizing performance, and driving
   innovation across web and mobile application projects.`,
->>>>>>> f7de53023008554aee2f3b08220f25ecf5077cb2
     email: "yashsonijaipur@gmail.com",
     phone: "+91 7375863649",
     address: "Jaipur, Rajasthan",
     github: "https://github.com/YashSoni321",
-<<<<<<< HEAD
-    facebook: 'https://www.facebook.com/yashsonii/',
-    linkedIn: "https://www.linkedin.com/in/yashsonii/",
-    twitter: 'https://twitter.com/yashsonii',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/yashsonii',
-    devUsername: "yashsonii"
-}
-=======
     facebook: "https://www.facebook.com/yashsonii/",
     linkedIn: "https://www.linkedin.com/in/yashsonii/",
     twitter: "https://www.linkedin.com/in/yashsonii/",
@@ -33,4 +22,3 @@ export const contactsData = {
     resume:
       "https://drive.google.com/file/d/1ICeLTgXLJOp91jmTrsox6t0zJ8lpSFnt/view?usp=sharing",
   };
->>>>>>> f7de53023008554aee2f3b08220f25ecf5077cb2
