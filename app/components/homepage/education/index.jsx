d602de663b0994ce8c3391246a114cc2004e4705
YashSoni321 <yashsonijaipur@gmail.com>
2025-01-19 22:30:20 +0530

    changes in test

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -4,8 +4,14 @@ import Image from "next/image";
 import { educations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+// import GlowCard from "../../helper/glow-card";
+import dynamic from "next/dynamic";
 import lottieFile from '/public/lottie/study.json';
+
+const GlowCard = dynamic(() => import("../../helper/glow-card"), {
+  ssr: false, // Disable SSR for this component
+});
+
 
 function Education() {
   return (
