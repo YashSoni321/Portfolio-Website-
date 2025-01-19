5b9c35d254229ab7ef30065ddc297bb7944777fa
YashSoni321 <yashsonijaipur@gmail.com>
2025-01-19 22:39:15 +0530

    changes in test

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
     // Ensure this code runs only in the browser
-    if (typeof document !== 'undefined') {
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
       const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
       const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
 
