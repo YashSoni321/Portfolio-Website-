932efd28075c3c5c90a96b4c6d3f2a0a0ad9dbf0
Yash-solvitine <yashsoni@heyneo.so>
2025-07-31 21:04:07 +0530

    seo changes

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,75 @@ import "./css/card.scss";
 import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Portfolio of Yash Soni - Software Developer",
   description:
-    "This is the portfolio of Yash Soni. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "Portfolio of Yash Soni, a self-taught full stack developer passionate about learning and collaboration.",
+  keywords: [
+    "Yash Soni",
+    "Software Engineer",
+    "Full Stack Developer",
+    "React Developer",
+    "Node.js Developer",
+    "Portfolio",
+    "Techie Yash",
+    "techie Yash",
+    "full stack developer",
+    "full stack developer portfolio",
+    "developer portfolio",
+    "web developer",
+    "web developer portfolio",
+    "software engineer portfolio",
+    "software engineer",
+    "software developer",
+    "software developer portfolio",
+    "developer",
+    "developer portfolio",
+    "web development",
+    "web development portfolio",
+    "software development",
+    "software development portfolio",
+    "open source",
+    "open source contributions",
+    "open source developer",
+    "open source portfolio",
+    "self-taught developer",
+    "self-taught software engineer",
+    "freelance developer",
+    "freelance software engineer",
+    "freelance portfolio",
+    "freelance web developer",
+    "freelance web development",
+    "freelance software development",
+    "freelance projects",
+    "freelance projects portfolio",
+  ],
+  openGraph: {
+    title: "Yash Soni | Full Stack Developer",
+    description:
+      "Portfolio of Yash Soni, a passionate software engineer open to collaborations and new opportunities.",
+    url: "https://techieyash.com/",
+    siteName: "Yash Soni Portfolio",
+    images: [
+      {
+        url: "https://techieyash.com/_next/image?url=%2Fprofile_pic.jpg&w=640&q=75", // Replace with real image
+        width: 1200,
+        height: 630,
+        alt: "Yash Soni Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Yash Soni | Full Stack Developer",
+    description:
+      "Portfolio of Yash Soni, a self-taught developer exploring new challenges.",
+    images: [
+      "https://techieyash.com/_next/image?url=%2Fprofile_pic.jpg&w=640&q=75",
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
